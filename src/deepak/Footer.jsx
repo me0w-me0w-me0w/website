0566a423c9800d44f9d5aca40b18b84f2041fe17
deepak-vishwakarma <deepakvishwakarma9870@gmail.com>
2023-08-12 23:50:29 +0530

    Added Footer

---
+++ src/deepak/Footer.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <h4>Contact Us</h4>
+            <ul>
+              <li>Somewhere on Earth 🌍</li>
+              <li>+9199999999</li>
+              <li>
+                deepakvishwakarma@duck.com / piyush@hotAF.me /
+                satyamchomu@someWhereOnEarth.milkyway
+              </li>
+            </ul>
+
+            <ul>
+              <li>11:00AM - 18:00PM</li>
+              <li>EveryDay (Except Saturday and Sunday) </li>
+            </ul>
+          </div>
+
+          <div>
+            <h4>Updates and News</h4>
+          </div>
+          <div>
+            <h4>Gallery</h4>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <a href="https://www.Instagram.com" title="Instagram">
+                {" "}
+              </a>
+              <a href="https://www.Facebook.com" title="Facebook">
+                {" "}
+              </a>
+              <a href="https://www.Twitter.com" title="Twitter">
+                {" "}
+              </a>
+            </div>
+            <div>
+              <p>© 2023 - All Rights Reserved</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
