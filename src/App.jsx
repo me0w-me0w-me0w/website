0a9fbae707a20d815990f936254daaa943dfc2a4
satyam <satyam@gmail.com>
2023-08-27 18:03:27 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./deepak/Footer";
 import Navbar from "./piyush/Navbar";
 import Discover from "./piyush/home_discover";
-import Home from "./Components/Home";
+import About from "./pages/About";
+import Blogs from "./pages/Blogs";
+import Careers from "./pages/Careers";
+import History from "./pages/History";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
     <div>
-    <Navbar/>
-    <Discover/>
-      {/* <Home /> */}
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+        {/* <Home /> */}
+        <Discover />
+        <Footer />
+      </Router>
     </div>
   );
 }
-
-export default App;
